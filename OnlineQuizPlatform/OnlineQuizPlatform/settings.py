76e4ed625f07fd2c87a6bc5b8a09d24dfbee6b45
Hristo Dzhevizov <dzhevizov@abv.bg>
2022-07-20 16:09:43 +0300

    Added whitenoise for staticfiles

--- OnlineQuizPlatform/OnlineQuizPlatform/settings.py
+++ OnlineQuizPlatform/OnlineQuizPlatform/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -143,6 +144,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
